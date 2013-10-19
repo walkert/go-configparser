0625fd3e773d8fb5992c969eb627ab6504fda2a4
Tony Walker <walkert.uk@gmail.com>
2013-10-19 17:11:55 -0400

    Add tests to check GetOption() with bad data

--- examples/configparser_example.go
+++ examples/configparser_example.go
@@ -48,4 +48,21 @@ func main() {
         value, _ := cd.GetOption("global", "master")
         fmt.Println("master:", value)
     }
+    // Try getting an option from an invalid section
+    fmt.Println("\nTry and run GetOption on an invalid section")
+    value, err := cd.GetOption("app2", "no option")
+    if err != nil {
+	    fmt.Println("Problem!", err)
+    } else {
+	    fmt.Println("Option ok:", value)
+    }
+
+    // Try getting an option from an valid section
+    fmt.Println("Try and run GetOption on an invalid option")
+    value, err = cd.GetOption("app1", "bindir")
+    if err != nil {
+	    fmt.Println("Problem!", err)
+	    os.Exit(1)
+    }
+    fmt.Println("Option ok:", value)
 }
